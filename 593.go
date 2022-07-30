e637c03328d347f8e2dd5040409ca4aac44766a5
zmzp123 <1156564544@qq.com>
2022-07-30 11:23:13 +0800

    952

--- 593.go
+++ 593.go
@@ -1,1 +1,35 @@
 package Question_by_day
+
+func validSquare(p1 []int, p2 []int, p3 []int, p4 []int) bool {
+	// fmt.Println(vertical(p1,p2,p3,p4))
+	// fmt.Println(equal(p1,p2,p3,p4))
+	// fmt.Println(zd(p1,p2,p3,p4))
+	f1 := vertical(p1, p2, p3, p4) && equal(p1, p2, p3, p4) && zd(p1, p2, p3, p4)
+	f2 := vertical(p1, p3, p2, p4) && equal(p1, p3, p2, p4) && zd(p1, p3, p2, p4)
+	f3 := vertical(p1, p4, p2, p3) && equal(p1, p4, p2, p3) && zd(p1, p4, p2, p3)
+	return f1 || f2 || f3
+}
+
+func vertical(p1 []int, p2 []int, p3 []int, p4 []int) bool {
+	y := (p2[1] - p1[1]) * (p4[1] - p3[1])
+	x := (p2[0] - p1[0]) * (p4[0] - p3[0])
+	return x == -y
+}
+
+func equal(p1 []int, p2 []int, p3 []int, p4 []int) bool {
+	d1 := (p2[1]-p1[1])*(p2[1]-p1[1]) + (p2[0]-p1[0])*(p2[0]-p1[0])
+	d2 := (p4[1]-p3[1])*(p4[1]-p3[1]) + (p4[0]-p3[0])*(p4[0]-p3[0])
+	if d1 == 0 {
+		return false
+	}
+	return d1 == d2
+}
+
+func zd(p1 []int, p2 []int, p3 []int, p4 []int) bool {
+	x1 := (p1[0] + p2[0]) / 2
+	y1 := (p1[1] + p2[1]) / 2
+	x2 := (p3[0] + p4[0]) / 2
+	y2 := (p3[1] + p4[1]) / 2
+	// fmt.Println(x1,x2,y1,y2)
+	return x1 == x2 && y1 == y2
+}
--- 952.go
+++ 952.go
@@ -1,1 +1,67 @@
 package Question_by_day
+
+// 用并查集来做
+
+type set struct {
+	parent, rank []int
+}
+
+func NewSet(n int) set {
+	parent := make([]int, n+1)
+	for i := 0; i <= n; i++ {
+		parent[i] = i
+	}
+	return set{parent, make([]int, n+1)}
+}
+
+func (s set) find(x int) int {
+	if s.parent[x] != x {
+		return s.find(s.parent[x])
+	}
+	return x
+}
+
+func (s set) merge(x, y int) {
+	xp, yp := s.find(x), s.find(y)
+	if xp == yp {
+		return
+	}
+	if s.rank[xp] >= s.rank[yp] {
+		s.parent[yp] = xp
+		if s.rank[xp] == s.rank[yp] {
+			s.rank[xp] += 1
+		}
+	} else {
+		s.parent[xp] = yp
+	}
+}
+
+func largestComponentSize(nums []int) int {
+	m := 0
+	for _, i := range nums {
+		m = max(m, i)
+	}
+	s := NewSet(m)
+	for _, num := range nums {
+		for i := 2; i*i <= num; i++ {
+			if num%i == 0 {
+				s.merge(num, i)
+				s.merge(num, num/i)
+			}
+		}
+	}
+	res := 0
+	cnt := make([]int, m+1)
+	for _, num := range nums {
+		cnt[s.find(num)] += 1
+		res = max(res, cnt[s.find(num)])
+	}
+	return res
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
