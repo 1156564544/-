2cc923654a2d35ec19cb6b6ae7e595634c2b7b68
zmzp123 <1156564544@qq.com>
2022-08-19 10:27:33 +0800

    Offer II 017

--- 1450.go
+++ 1450.go
@@ -1,1 +1,11 @@
 package Question_by_day
+
+func busyStudent(startTime []int, endTime []int, queryTime int) (res int) {
+	n := len(startTime)
+	for i := 0; i < n; i++ {
+		if startTime[i] <= queryTime && endTime[i] >= queryTime {
+			res += 1
+		}
+	}
+	return
+}
