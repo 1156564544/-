794d8ecaec917e5eafe3b55bcd1254b6038626b7
1156564544 <1156564544@qq.com>
2022-08-26 13:00:07 +0800

    1464

---
+++ 1464.go
@@ -0,0 +1,17 @@
+package Question_by_day
+
+func maxProduct(nums []int) int {
+	max1, max2 := nums[0], nums[1]
+	if max1 < max2 {
+		max1, max2 = max2, max1
+	}
+	for i := 2; i < len(nums); i++ {
+		if nums[i] > max1 {
+			max2 = max1
+			max1 = nums[i]
+		} else if nums[i] > max2 {
+			max2 = nums[i]
+		}
+	}
+	return (max1 - 1) * (max2 - 1)
+}
