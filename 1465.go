7e96e303230dee2e1442ec48c9710c393b58a6fe
zmzp123 <1156564544@qq.com>
2022-09-01 10:20:10 +0800

    1475

--- 1465.go
+++ 1465.go
@@ -1,1 +1,24 @@
 package Question_by_day
+
+import "sort"
+
+func maxArea(h int, w int, horizontalCuts []int, verticalCuts []int) int {
+	sort.Ints(horizontalCuts)
+	sort.Ints(verticalCuts)
+	horizontalCuts = append(horizontalCuts, h)
+	verticalCuts = append(verticalCuts, w)
+	h_m, w_m := horizontalCuts[0], verticalCuts[0]
+	for i := len(horizontalCuts) - 1; i > 0; i-- {
+		horizontalCuts[i] = horizontalCuts[i] - horizontalCuts[i-1]
+		if h_m < horizontalCuts[i] {
+			h_m = horizontalCuts[i]
+		}
+	}
+	for i := len(verticalCuts) - 1; i > 0; i-- {
+		verticalCuts[i] = verticalCuts[i] - verticalCuts[i-1]
+		if w_m < verticalCuts[i] {
+			w_m = verticalCuts[i]
+		}
+	}
+	return (h_m * w_m) % (1000000007)
+}
---
+++ 1475.go
@@ -0,0 +1,13 @@
+package Question_by_day
+
+func finalPrices(prices []int) []int {
+	for i := 0; i < len(prices); i++ {
+		for j := i + 1; j < len(prices); j++ {
+			if prices[j] <= prices[i] {
+				prices[i] -= prices[j]
+				break
+			}
+		}
+	}
+	return prices
+}
