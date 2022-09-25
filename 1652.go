920c0f87a046a2822d04afeefee47964fa99cd94
zmzp123 <1156564544@qq.com>
2022-09-25 23:04:46 +0800

    788

--- 1652.go
+++ 1652.go
@@ -1,1 +1,33 @@
 package Question_by_day
+
+func decrypt(code []int, k int) []int {
+	n := len(code)
+	if k == 0 {
+		for i := 0; i < n; i++ {
+			code[i] = 0
+		}
+	} else if k > 0 {
+		for i := 0; i < k; i++ {
+			code = append(code, code[i])
+		}
+		for i := 0; i < n; i++ {
+			sum := 0
+			for j := i + 1; j <= i+k; j++ {
+				sum += code[j]
+			}
+			code[i] = sum
+		}
+		code = code[:n]
+	} else {
+		code = append(code, code[n+k:]...)
+		for i := n - 1; i >= 0; i-- {
+			sum := 0
+			for j := i - 1; j >= i+k; j-- {
+				sum += code[(j+n-k)%(n-k)]
+			}
+			code[i] = sum
+		}
+		code = code[:n]
+	}
+	return code
+}
